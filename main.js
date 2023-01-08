2dd03a6ef39bade4a15a8791c0eef52065285ce6
Od100 <90208081+Od100@users.noreply.github.com>
2023-01-07 20:51:41 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quallity:90
+});
+camera=document.getElementById("Camera");
+Webcam.attach('#Camera');
+function take_snapshot(){
+    Webcam.snap(function(data_uri)
+    {
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+console.log('ml5 version:',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ZS83-CooJ/model.json',modelLoaded);
+function modelLoaded(){
+    console.log('Model Loaded!');
+    
+}function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+function gotResult(error, results){
+     if (error) {
+        console.error(error);
+     }
+     else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+     }
+}
